a0dd0cfa5f48b0bf59114ef88fa369af493026e3
Subin Sabu <74660211+subezio-whitehat@users.noreply.github.com>
2021-12-20 11:29:53 +0530

    cycle 3
    cycle 3

---
+++ cycle 3/c3p5.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["walking", "cycling", "car", "bus","train"])
+y = np.array([29,15,35,18,3])
+
+plt.bar(x,y, color="#4CAF50",width=0.1)
+plt.show()
