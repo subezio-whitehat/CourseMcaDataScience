3ba1aeac7f33f56c7c6cc19783960d4e73be76cb
Subin Sabu <74660211+subezio-whitehat@users.noreply.github.com>
2022-01-04 12:19:50 +0530

    Update c3p4.py

--- cycle 3/c3p4.py
+++ cycle 3/c3p4.py
@@ -1,20 +1,15 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
+x = np.array(["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"])
+y1 =  np.array([173,153,195,147,120,144,148,109,174,130,172,131])
+y2 = np.array([189,189,105,112,173,109,151,197,174,145,177,161])
+y3 =  np.array([185,185,126,134,196,153,112,133,200,145,167,110])
 
+fig, ax = plt.subplots()
 
-plt.title("Sales Data")
-plt.xlabel("Months of Year")
-plt.ylabel("Sale of Food")
-x = np.array([173,153,195,147,120,144,148,109,174,130,172,131])
-y = np.array([173,153,195,147,120,144,148,109,174,130,172,131])
-plt.scatter(x,y, color = 'hotpink')
-
-x = np.array([185,185,126,134,196,153,112,133,200,145,167,110])
-y = np.array([185,185,126,134,196,153,112,133,200,145,167,110])
-plt.scatter(x, y, color = 'yellow')
-
-
-x = np.array([189,189,105,112,173,109,151,197,174,145,177,161])
-y = np.array([189,189,105,112,173,109,151,197,174,145,177,161])
-plt.scatter(x, y, color = 'blue')
+ax.plot(x, y1,label="affordable segment",ls=":")
+ax.plot(x, y2,label="luxuary segment",ls="-.")
+ax.plot(x,y3,label = "super segment")
+ax.legend()
 plt.show()
